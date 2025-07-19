7690355c0759a4b15748a1b0dbbbcccd08e3fabe
hbohorqueza <k166.hectobohorquez@gmail.com>
2025-07-18 21:51:51 -0500

    Update with function Random

--- select_modules.py
+++ select_modules.py
@@ -17,3 +17,22 @@ print(ad == 90.)
 print(ar == pi / 2.)
 print(sin(ar) / cos(ar) == tan(ar))
 print(asin(sin(ar)) == ar)
+
+
+###  Funciones selectas del módulo random    #####
+
+## produce un número flotante x entre el rango (0.0, 1.0) - en otras palabras: (0.0 <= x < 1.0).
+
+from random import random
+
+for i in range(5):
+    print(random())
+
+
+
+from random import random, seed
+
+seed(0)
+
+for i in range(5):
+    print(random())
